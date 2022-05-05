0605e70da65280d3fef5ff1ba349ee0fd959370c
Jesper Hauch <s174227@student.dtu.dk>
2022-05-05 14:18:28 +0200

    make request file

--- request.py
+++ request.py
@@ -1,5 +1,6 @@
+
 import requests
-endpoint = "https://models.grace-dtu.2021.services/seldon/project-spaceembeddings/recoms/api/v0.1/predictions"
+endpoint = "https://models.grace-dtu.2021.services/seldon/project-spaceembeddings/recommender-system/api/v0.1/predictions"
 
 headers = {'Grace-Client-Secret': 'c0de6747-ffb6-4023-913f-53c8222435bb'}
 IDs = ['101742583391038750118','100574642292837870712']
