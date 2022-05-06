65aa6b4f4c08657e6e2fc7950b516db10a2b7579
Jesper Hauch <s174227@student.dtu.dk>
2022-05-06 14:10:33 +0200

    DISCRETE VIRKER

--- request.py
+++ request.py
@@ -5,7 +5,8 @@ endpoint = "https://models.grace-dtu.2021.services/seldon/project-spaceembedding
 headers = {'Grace-Client-Secret': 'c0de6747-ffb6-4023-913f-53c8222435bb'}
 IDs = ['101742583391038750118','100574642292837870712']
 Ratings = [4,2]
-payload = {"data": {"ndarray": [IDs, Ratings]}}
+payload = {"data": {"names": ["feature-1", "feature-2"],
+                    "ndarray": [IDs, Ratings]}}
 response = requests.post(endpoint, json = payload, headers = headers)
 
 print(response.status_code)
