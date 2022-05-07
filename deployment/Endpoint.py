ed1a28daacbdd058f71b90a822c3b1e40a2e7578
Jesper Hauch <s174227@student.dtu.dk>
2022-05-07 17:54:11 +0200

    NLP classifier rerun

--- deployment/Endpoint.py
+++ deployment/Endpoint.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
-import os
 
 class Endpoint(object):
     """
@@ -11,6 +10,7 @@ class Endpoint(object):
     """
 
     def __init__(self):
+        # Initializations - places to map given gPlaceIds to grid cell and load of embeddings
         self.places = pd.read_csv(ROOT_DIR + "/serialized/places_final.csv")
         self.embeddings = pd.DataFrame(pickle.load(open(ROOT_DIR + "/serialized/continuous_embeddings.pkl", "rb"))).T
 
@@ -22,6 +22,7 @@ class Endpoint(object):
         visited = self.places.loc[self.places.gPlusPlaceId.isin(IDs)][['gPlusPlaceId','city','Grid']]
         # Add grid cell, city to user
         user = user.merge(visited, left_on='IDs', right_on='gPlusPlaceId')
+        # Ensure rating is numeric
         user['Rating'] = pd.to_numeric(user['Rating'])
         # Save location user is from
         city = user.city.unique()[0]
@@ -30,7 +31,7 @@ class Endpoint(object):
         avg_grid_rating = pd.DataFrame(user.groupby('Grid').Rating.mean())
         avg_user_rating = self.embeddings.loc[avg_grid_rating.index].values.T @ avg_grid_rating.values
 
-        # Calculate the cosine similiary
+        # Calculate the cosine similarity for embeddings in different city 
         recommendation = pd.DataFrame(index = [i for i in self.embeddings.index if city[0] not in i])
         for grid in recommendation.index:
             recommendation.loc[grid, 'cosine_similarity'] = cosine_similarity(avg_user_rating.reshape(1,-1),self.embeddings.loc[grid].values.reshape(1,-1))
